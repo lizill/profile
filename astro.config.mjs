0b80e4cf6ae85b3e2e52035f55f285800f85c6ab
lizill <lizill@naver.com>
2026-06-29 01:38:44 +0900

    chore: base path を変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-const base = process.env.PUBLIC_SITE_BASE_PATH ?? "/my-profile-site";
+const base = process.env.PUBLIC_SITE_BASE_PATH ?? "/profile";
 const site = process.env.PUBLIC_SITE_URL ?? "https://lizill.github.io";
 
 export default defineConfig({
